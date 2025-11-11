82e0ea6d37961be43229b8ec158a5b3f20f2f810
David Alvarez Iglesias <daalvarezig@gmail.com>
2025-11-11 03:37:48 +0000

    Updated City and Country

--- src/js/app.js
+++ src/js/app.js
@@ -33,8 +33,9 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
+          <h1>${variables.name || "David"} ${variables.lastName ||
+    "Álvarez"}</h1> 
+          <h2>${variables.role || "Full Stack"}</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
